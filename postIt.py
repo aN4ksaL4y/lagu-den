55b703558946fdd91ba7b4959a5dc91c515ebd9f
Your Name <you@example.com>
2024-10-13 17:56:25 +0700

    nambah post data flow

--- postIt.py
+++ postIt.py
@@ -1,29 +1,42 @@
-from main import requests, json
+from main import requests, json, time
 
-url = "https://api.github.com/gists"
+url = "https://api.github.com/gists/457b453bb2e74d8bd23d74176f842ada"
+json_file_path = 'recently_played_songs.json'
+
 
 # Load the JSON configuration file
 with open('config.json', 'r') as file:
     config = json.load(file)
 
-headers = {
-    "Accept": "application/vnd.github+json",
-    "Authorization": f"Bearer {config['ghp']}",
-    "X-GitHub-Api-Version": "2022-11-28"
-}
-data = {
-    "description": "Recently Played Songs",
-    "public": True,
-    "files": {
-        "recently_played_songs.json": {
-            "content": "Hello World of Gist!"
+def load_data_from_json(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+
+if __name__=="__main__":
+    while True:
+        new_content = json.dumps(load_data_from_json(json_file_path), indent=2)
+        headers = {
+            "Accept": "application/vnd.github+json",
+            "Authorization": f"Bearer {config['ghp']}",
+            "X-GitHub-Api-Version": "2022-11-28"
         }
-    }
-}
+        data = {
+            "description": "Recently Played Songs",
+            "public": True,
+            "files": {
+                "recently_played.json": {
+                    "content": new_content
+                }
+            }
+        }
 
-response = requests.post(url, headers=headers, json=data)
+        response = requests.patch(url, headers=headers, json=data)
+        response.raise_for_status()  # Raise an error for bad responses
 
-if response.status_code == 201:
-    print("Gist created successfully:", response.json())
-else:
-    print("Failed to create gist:", response.status_code, response.text)
+        if response.status_code == 201:
+            print("Gist created successfully:", response.json())
+        else:
+            print("Failed to create gist:", response.status_code, response.text)
+
+        time.sleep(60)
